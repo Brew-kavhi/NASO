f59d5fdb9622b3f28777dbaeacaa3024aba0b101
Marius Goehring <goehrims@ipvs.uni-stuttgart.de>
2023-10-29 12:27:38 +0100

    feat(page): implementing details page

--- code/NASO/runs/views/RunDetails.py
+++ code/NASO/runs/views/RunDetails.py
@@ -2,6 +2,7 @@ import json
 
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView, View
+from neural_architecture.models.AutoKeras import AutoKerasRun
 
 from naso.celery import get_celery_task_state
 from naso.models.page import PageSetup
@@ -29,5 +30,8 @@ class AutoKerasRunDetails(TemplateView):
     page = PageSetup(title="Experimente", description="Details")
     context = {"page": page.get_context()}
 
-    def get(self, request, *args, **kwargs):
+    def get(self, request,  *args, **kwargs):
+        run = AutoKerasRun.objects.get(pk=kwargs['pk'])
+        self.context['run'] = run
+        print(kwargs['pk'])
         return self.render_to_response(self.context)
