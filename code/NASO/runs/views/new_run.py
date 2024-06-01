dd311b9bb3960b19a6a62cc4113559c4d0b95eb8
Marius Goehring <marius@mariusgoehring.de>
2024-06-01 14:11:52 +0200

    fix(runs): empty inference gpus

--- code/NASO/runs/views/new_run.py
+++ code/NASO/runs/views/new_run.py
@@ -470,7 +470,11 @@ class NewRun(TemplateView):
                         )
 
                 workers = ast.literal_eval(form.cleaned_data["gpu"])
-                inference_workers = ast.literal_eval(form.cleaned_data["inference_gpu"])
+                inference_workers = []
+                if len(form.cleaned_data["inference_gpu"]) > 0:
+                    inference_workers = ast.literal_eval(
+                        form.cleaned_data["inference_gpu"]
+                    )
                 for idx, worker in enumerate(workers):
                     queue, gpu = worker.split("|")
                     training = NetworkTraining()
